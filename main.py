d611c0c42ff5937232fa072ffa70019d855a1837
pedro ramos <pedro.ramos9@hotmail.com>
2022-05-29 16:53:28 +0100

    correlation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+from asyncio.windows_events import NULL
 from matplotlib import pyplot as plt
 from operator import index
 
@@ -6,18 +7,17 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import scipy.stats as st
-# fazer a matriz de correlaoçao o heatmap,aplicar o modelo de relaçao linear
 def readfile(file,cols):
-    return pd.read_csv(file, usecols=cols)
+    if cols!=NULL:
+        return pd.read_csv(file, usecols=cols)
+    else:
+        return pd.read_csv(file)
 
-dataf=readfile('pokemon.csv',cols=[1,2,3,4])
+dataf=readfile('./projeto/assets/winequality-red.csv',cols=[0,1,2,3,4,5,6,7,8,9,10,11])
 
 print(dataf)
-# for i in dataf:
-#     print(i,st.norm.interval(0.9, loc=dataf[i].mean(0),scale=dataf[i].std(0)))
+print(dataf.corr())
 
-# sns.displot(dataf,kind="kde")
-# for i in dataf:
-#     sns.displot(dataf[i],kind="kde")
-# plt.show()
-
+heatmap=sns.heatmap(dataf.corr(),annot=True,cmap="Greens")
+heatmap.set_title('Correlation Heatmap', fontdict={'fontsize':12}, pad=12);
+plt.show()
